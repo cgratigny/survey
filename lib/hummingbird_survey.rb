a6b57004c327c7f27c1fe8f1e09a3424e3a40744
Chris Gratigny <chrisg@ibethel.org>
2019-09-23 11:51:00 -0700

    Models.

--- lib/hummingbird_survey.rb
+++ lib/hummingbird_survey.rb
@@ -1,9 +1,9 @@
 require 'rails'
-require "lib/hummingbird_survey/models/survey_item.rb"
-require "lib/hummingbird_survey/models/survey_note.rb"
-require "lib/hummingbird_survey/models/survey_page.rb"
-require "lib/hummingbird_survey/models/survey_questions.rb"
-require "lib/hummingbird_survey/models/survey_section.rb"
+require "hummingbird_survey/models/survey_item.rb"
+require "hummingbird_survey/models/survey_note.rb"
+require "hummingbird_survey/models/survey_page.rb"
+require "hummingbird_survey/models/survey_questions.rb"
+require "hummingbird_survey/models/survey_section.rb"
 
 module HummingbirdSurvey
   def self.hi
