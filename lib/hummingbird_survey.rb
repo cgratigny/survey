7e1e2a49ddaab0ce09caca3ccbec26cb6444df08
Chris Gratigny <chrisg@ibethel.org>
2019-09-23 11:39:58 -0700

    Try including each file.

--- lib/hummingbird_survey.rb
+++ lib/hummingbird_survey.rb
@@ -1,5 +1,9 @@
 require 'rails'
-require "lib/hummingbird_survey/models/*"
+require "lib/hummingbird_survey/models/survey_item"
+require "lib/hummingbird_survey/models/survey_note"
+require "lib/hummingbird_survey/models/survey_page"
+require "lib/hummingbird_survey/models/survey_questions"
+require "lib/hummingbird_survey/models/survey_section"
 
 module HummingbirdSurvey
   def self.hi
