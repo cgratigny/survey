b7c251ef45c507477b887431c1185d66f59a1363
Chris Gratigny <chrisg@ibethel.org>
2019-09-23 12:07:27 -0700

    Update includes.

--- lib/hummingbird_survey.rb
+++ lib/hummingbird_survey.rb
@@ -1,10 +1,11 @@
 require 'rails'
-require "hummingbird_survey/models/itemable.rb"
-require "hummingbird_survey/models/notable.rb"
-require "hummingbird_survey/models/pagable.rb"
-require "hummingbird_survey/models/sectionable.rb"
-require "hummingbird_survey/models/surveyable.rb"
-require "hummingbird_survey/models/questionable.rb"
+
+require 'hummingbird_survey/models/itemable.rb'
+require 'hummingbird_survey/models/notable.rb'
+require 'hummingbird_survey/models/pagable.rb'
+require 'hummingbird_survey/models/sectionable.rb'
+require 'hummingbird_survey/models/surveyable.rb'
+require 'hummingbird_survey/models/questionable.rb'
 
 module HummingbirdSurvey
   def self.hi
