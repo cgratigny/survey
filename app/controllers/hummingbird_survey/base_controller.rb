5011a0e6886c3779682eb40b63158c18a67bf275
Chris Gratigny <chrisg@ibethel.org>
2019-09-27 13:18:04 -0700

    Controllers

---
+++ app/controllers/hummingbird_survey/base_controller.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# TODO: Make this extend a configurable base controller
+class HummingbirdSurvey::BaseController < Staff::BaseController
+
+end
--- app/controllers/hummingbird_survey_controller.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-# All Devise controllers are inherited from here.
-class HummingbirdSurveyController < Staff::BaseController
-  
-end
