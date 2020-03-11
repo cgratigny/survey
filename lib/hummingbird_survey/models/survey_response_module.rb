5a8054577bee42ab4a2edc3dfd6fec4efcf8cb7c
Chris Gratigny <chrisg@ibethel.org>
2020-03-11 11:46:32 -0700

    Check if completed on survey response.

--- lib/hummingbird_survey/models/survey_response_module.rb
+++ lib/hummingbird_survey/models/survey_response_module.rb
@@ -6,5 +6,10 @@ module HummingbirdSurvey
       belongs_to :survey
       belongs_to :surveyed, polymorphic: true
     end
+
+    def completed?
+      survey.completed_by?(surveyed)
+    end
+
   end
 end
